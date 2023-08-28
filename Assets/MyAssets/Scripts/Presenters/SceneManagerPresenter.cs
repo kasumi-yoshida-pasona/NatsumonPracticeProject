acb883e47fb610a1e7f223a99762eed42bb08b6f
kasumi-yoshida-pasona <110513233+kasumi-yoshida-pasona@users.noreply.github.com>
2023-08-28 16:57:51 +0900

    [Add] SceneのMVPファイルを作成

---
+++ Assets/MyAssets/Scripts/Presenters/SceneManagerPresenter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerPresenter : MonoBehaviour
+{
+    /// <summary>
+    /// ロード実行状況の通知を受けてViewに反映するPresenter
+    /// </summary>
+}
--- Assets/MyAssets/Scripts/Views/TitleLoadingView.cs
+++ Assets/MyAssets/Scripts/Views/TitleLoadingView.cs
@@ -12,4 +12,3 @@ namespace TitleScene.Views
         /// </summary>
     }
 }
-
