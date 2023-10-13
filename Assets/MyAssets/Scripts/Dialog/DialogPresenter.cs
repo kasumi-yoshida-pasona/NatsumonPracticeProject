5888a630bcd59034b2c962e7eefccd22d8e570f7
kasumi-yoshida-pasona <110513233+kasumi-yoshida-pasona@users.noreply.github.com>
2023-10-13 17:30:31 +0900

    20231013 一旦コミット

--- Assets/MyAssets/Scripts/Dialog/DialogPresenter.cs
+++ Assets/MyAssets/Scripts/Dialog/DialogPresenter.cs
@@ -12,7 +12,9 @@ namespace natsumon
         [SerializeField] private Canvas parent;
         // 表示するダイアログ
         [SerializeField] private DialogView dialog;
+        [SerializeField] GameObject dialogPrefab;
         private DialogModel dialogModel;
+        private ButtonModel buttonModel;
 
         // Button
         private ButtonView exitBtn;
@@ -20,6 +22,7 @@ namespace natsumon
 
         private void Awake() {
             dialogModel = new DialogModel();
+            buttonModel = new ButtonModel();
             exitBtn = dialog.exitBtn;
             cancelBtn = dialog.cancelBtn;
         }
@@ -27,21 +30,25 @@ namespace natsumon
         void OnDestroy()
         {
             dialogModel.Dispose();
+            buttonModel.Dispose();
         }
 
         private void Start() {
-            cancelBtn.OnSelectAsObservable()
-                .Subscribe(targetBtn => {
-                    dialogModel.StoreSelectedBtn(targetBtn);
-                    Debug.Log(targetBtn);
-                })
-                .AddTo(this);
 
-            exitBtn.OnSelectAsObservable()
-                .Subscribe(targetBtn => {
-                    dialogModel.StoreSelectedBtn(targetBtn);
-                })
-                .AddTo(this);
+
+
+            // cancelBtn.OnSelectAsObservable()
+            //     .Subscribe(targetBtn => {
+            //         dialogModel.StoreSelectedBtn(targetBtn);
+            //         Debug.Log(targetBtn);
+            //     })
+            //     .AddTo(this);
+
+            // exitBtn.OnSelectAsObservable()
+            //     .Subscribe(targetBtn => {
+            //         dialogModel.StoreSelectedBtn(targetBtn);
+            //     })
+            //     .AddTo(this);
 
             // modelで選択されたボタン情報が変更されたときの処理
             dialogModel.SelectedBtn.Subscribe(selectedBtn => {
@@ -53,6 +60,15 @@ namespace natsumon
 
         public void SetOnFinishBtnPressed()
         {
+            Debug.Log("helloS");
+            var obj = Instantiate(dialogPrefab, null);
+            var dialogView = obj.GetComponent<DialogView>();
+            dialogView.cancelBtn.OnSelectAsObservable()
+            .Subscribe(b => {
+                Debug.Log(b);
+                buttonModel.StoreSelectedBtn(b);
+            }).AddTo(obj);
+
             // // ゲーム終了確認ダイアログ表示
             dialog.ShowDialog(parent);
             // ダイアログが表示されたことをModelに通知
