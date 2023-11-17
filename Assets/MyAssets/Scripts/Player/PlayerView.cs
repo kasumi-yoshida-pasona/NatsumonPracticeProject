619efe54913b7cba1c4cec09a582068cdd9ee946
kasumi-yoshida-pasona <kasyoshida@fab.pasona.tech>
2023-11-17 17:48:49 +0900

    [Add] InputSystemのAWSDを設定（まだ移動できない状態）

--- Assets/MyAssets/Scripts/Player/PlayerView.cs
+++ Assets/MyAssets/Scripts/Player/PlayerView.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,13 +6,41 @@ namespace natsumon
 {
     public class PlayerView : MonoBehaviour
     {
-        [SerializeField] private Transform playerPosition;
+        // Actionをインスペクターから編集できるようにする
+        [SerializeField] private InputAction _action;
 
-        private void Awake()
+        // 有効化
+        private void OnEnable()
         {
-            Debug.Log(playerPosition);
-            var current = Keyboard.current;
-            Debug.Log(current);
+            // Actionのコールバックを登録
+            _action.performed += OnMove;
+            _action.canceled += OnMove;
+
+            // InputActionを有効化
+            // これをしないと入力を受け取れないことに注意
+            _action?.Enable();
+        }
+
+        // 無効化
+        private void OnDisable()
+        {
+            // Actionのコールバックを解除
+            _action.performed -= OnMove;
+            _action.canceled -= OnMove;
+
+            // 自身が無効化されるタイミングなどで
+            // Actionを無効化する必要がある
+            _action?.Disable();
+        }
+
+        // コールバックを受け取ったときの処理
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            // Actionの入力値を読み込む
+            var value = context.ReadValue<Vector2>();
+
+            // 入力値をログ出力
+            Debug.Log($"移動量 : {value}");
         }
     }
 }
