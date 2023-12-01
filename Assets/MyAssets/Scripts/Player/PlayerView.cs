4a590bdc3910617b5ea27e812600b39810a62bfe
kasumi-yoshida-pasona <kasyoshida@fab.pasona.tech>
2023-12-01 18:25:31 +0900

    斜め45度の方向にのみ移動している状態

--- Assets/MyAssets/Scripts/Player/PlayerView.cs
+++ Assets/MyAssets/Scripts/Player/PlayerView.cs
@@ -7,6 +7,7 @@ namespace natsumon
 {
     public class PlayerView : MonoBehaviour
     {
+        [SerializeField] private PlayerFollower playerFollower;
         CharacterController characterController;
         PlayerInput playerInput;
         Animator animator;
@@ -18,45 +19,50 @@ namespace natsumon
 
         bool isRunning = false;
         Vector2 input = Vector2.zero;
+        float cameraDirection = 0f;
+
 
         void Start()
         {
             characterController = GetComponent<CharacterController>();
             playerInput = GetComponent<PlayerInput>();
             animator = GetComponent<Animator>();
+
+            playerInput.enabled = true;
         }
 
         void Update()
         {
-            var velocity = Vector3.zero;
+            Vector3 inputDirection = new Vector3(input.x, 0, input.y);
+            // 方向キーの入力がなかったらReturn
+            if (inputDirection == Vector3.zero) return;
 
+            // キャラクターの向き
+            // 入力されたZ軸方向とPlayerFollowerの正面方向、入力されたX軸方向とplayerFollowerの前後方向を正規化した値
+            Vector3 nextDirection = (playerFollower.transform.forward * inputDirection.x + playerFollower.transform.right * inputDirection.x).normalized;
 
-            // 方向キーの入力があったら、その向きにキャラクターを回転させる
-            if (input.magnitude != 0)
-            {
+            // キャラクターの角度をDirectionの方向へ変える
+            // 現在の位置に角度を加算すればできるはず
+            var nextPos = transform.position + nextDirection;
+            this.transform.LookAt(nextPos);
 
-                Vector3 playerDirection = Vector3.Normalize(new Vector3(input.x, 0, input.y));
-                Quaternion characterTargetRotation = Quaternion.LookRotation(playerDirection);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, characterTargetRotation, rotationSpeed * Time.deltaTime);
-            }
-
-            // 移動速度の計算
-            float speedByStick = input.magnitude;
-            float moveSpeed = walkSpeed * speedByStick;
-
+            // 移動速度
+            float moveSpeed = walkSpeed;
             if (isRunning)
-            {
                 moveSpeed *= sprintSpeedUpRatio;
-            }
-            velocity = transform.forward * moveSpeed;
-
-            // アニメーションの速度設定
-            animator.SetFloat("MoveSpeed", moveSpeed);
 
             // キャラクターの移動
-            characterController.Move(velocity * Time.deltaTime);
+            characterController.Move(nextDirection * Time.deltaTime * moveSpeed);
 
-            Debug.Log("isRunnging : " + isRunning.ToString());
+
+            // カメラの向きが０以外であれば入力された方向に1度角度変更（変更する角度は後で調整）
+            // カメラのtransform更新
+            playerFollower.transform.position = this.transform.position;
+
+
+
+            // // アニメーションの速度設定
+            // animator.SetFloat("MoveSpeed", moveSpeed);
         }
 
         public void OnMove(InputValue value)
@@ -66,6 +72,12 @@ namespace natsumon
         public void OnSprint(InputValue value)
         {
             isRunning = value.isPressed;
+        }
+        public void OnRotateCamera(InputValue value)
+        {
+            cameraDirection = value.Get<float>();
+            Debug.Log(cameraDirection);
+            // 1が右、０が入力なし、−１が左
         }
     }
 }
---
+++ Assets/PlayerFollower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace natsumon
+{
+    public class PlayerFollower : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        // void Start()
+        // {
+        //     Debug.Log(this.transform);
+        // }
+
+        // private void Update()
+        // {
+        //     Debug.Log(this.transform.position);
+        // }
+
+    }
+}
